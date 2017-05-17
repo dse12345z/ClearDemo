30995ef6c285d5ae1d46178b08a201181ff8171f
daisuke <daisuke@DaisukedeMacBook-Air.local>
2017-05-17 17:39:51 +0800

    fix

--- ClearDemo/MainViewController.m
+++ ClearDemo/MainViewController.m
@@ -8,30 +8,46 @@
 
 #import "MainViewController.h"
 
-@interface MainViewController ()
-
-@end
-
 @implementation MainViewController
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+    UIBezierPath *bezierPath = [[UIBezierPath alloc] init];
 
-/*
-#pragma mark - Navigation
+    [bezierPath moveToPoint:CGPointMake(0, 0)];;;
+    
+    
+    [bezierPath addLineToPoint: CGPointMake(50, 50)];
+    
+    
+    CAShapeLayer    * shapeLayer = [[CAShapeLayer alloc] init];;;;;;;;
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    shapeLayer.path =bezierPath.CGPath;
+    
+    
+    
+    
+    
+    shapeLayer.strokeColor = [UIColor redColor].CGColor;
+    
+    
+    
+    
+    
+    
+    shapeLayer.lineWidth = 1.0;
+    
+    
+    
+    
+    
+    
+    [self.view.layer addSublayer:shapeLayer];
+    
+    
 }
-*/
 
 @end
